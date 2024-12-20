50a2ff2033bfd3507c237ea8be0e395244ec4b27
Suat Kuran <suatkuran@gmail.com>
2024-12-20 18:31:18 +0300

    Initial commit

---
+++ SozcuMainPage/Pages/Index.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SozcuMainPage.Services;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using static SozcuMainPage.Services.ElasticSearchService;
+
+namespace SozcuMainPage.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        private readonly ElasticSearchService _elasticsearchService;
+
+        public IndexModel(ElasticSearchService elasticsearchService)
+        {
+            _elasticsearchService = elasticsearchService;
+        }
+
+        public List<MyDocument> Documents { get; set; }
+        public string Query { get; set; }
+
+        public async Task OnGetAsync(string query)
+        {
+            Query = query;
+
+            if (!string.IsNullOrWhiteSpace(query))
+            {
+                Documents = await _elasticsearchService.SearchDocumentsAsync(query);
+            }
+            else
+            {
+                Documents = await _elasticsearchService.GetDocumentsAsync();
+            }
+        }
+
+    }
+}
---
+++ SozcuMainPage/Services/ElasticSearchService.cs
@@ -0,0 +1,85 @@
+﻿using Nest;
+using Elasticsearch.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+namespace SozcuMainPage.Services
+{
+    public class ElasticSearchService
+    {
+        private readonly ElasticLowLevelClient _client;
+
+        public ElasticSearchService()
+        {
+            var settings = new ConnectionConfiguration(new Uri("http://localhost:9200"));
+            _client = new ElasticLowLevelClient(settings);
+        }
+
+        public async Task<List<MyDocument>> GetDocumentsAsync()
+        {
+            var response = await _client.SearchAsync<StringResponse>("sozcu-main-page", PostData.Serializable(new { query = new { match_all = new { } } }));
+            var documents = JsonConvert.DeserializeObject<SearchResponse>(response.Body);
+            return documents?.Hits?.HitsList?.Select(hit => hit.Source).ToList() ?? new List<MyDocument>();
+        }
+
+        public async Task<List<MyDocument>> SearchDocumentsAsync(string query)
+        {
+            var response = await _client.SearchAsync<StringResponse>("sozcu-main-page", PostData.Serializable(new
+            {
+                query = new
+                {
+                    match = new
+                    {
+                        title = new { query = query }  
+                    }
+                }
+            }));
+
+            Console.WriteLine(response.Body);
+
+            var documents = JsonConvert.DeserializeObject<SearchResponse>(response.Body);
+            return documents?.Hits?.HitsList?.Select(hit => hit.Source).ToList() ?? new List<MyDocument>();
+        }
+
+        private class SearchResponse
+        {
+            [JsonProperty("hits")]
+            public Hits Hits { get; set; }
+        }
+
+        private class Hits
+        {
+            [JsonProperty("total")]
+            public Total Total { get; set; }
+
+            [JsonProperty("hits")]
+            public List<Hit> HitsList { get; set; }
+        }
+
+        private class Hit
+        {
+            [JsonProperty("_source")]
+            public MyDocument Source { get; set; }
+        }
+
+        private class Total
+        {
+            [JsonProperty("value")]
+            public long Value { get; set; }
+
+            [JsonProperty("relation")]
+            public string Relation { get; set; }
+        }
+
+
+        public class MyDocument
+        {
+            public string Title { get; set; }
+            public string Link { get; set; }
+            public DateTime Timestamp { get; set; }
+        }
+    }
+}
+
